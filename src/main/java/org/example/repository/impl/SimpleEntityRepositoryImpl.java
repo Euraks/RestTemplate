3779294e3d15db25dbfb869a94625dcee1aac2de
Yaroslav Agapov <euraks@yandex.ru>
2023-09-14 17:33:22 +0500

    feat: rename DTO

--- src/main/java/org/example/repository/impl/SimpleEntityRepositoryImpl.java
+++ src/main/java/org/example/repository/impl/SimpleEntityRepositoryImpl.java
@@ -22,11 +22,11 @@ public class SimpleEntityRepositoryImpl implements SimpleEntityRepository {
     @Override
     public SimpleEntity findById(UUID uuid) {
         String sql = "SELECT uuid,description FROM simpleentity WHERE uuid='"+uuid.toString()+"'";
-        // Здесь используем try with resources
         try (Connection connection = connectionManager.getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement( sql )){
             ResultSet resultSet = preparedStatement.executeQuery();
             List<SimpleEntity> simpleEntityList = getSimpleEntiysList( resultSet );
+
             return simpleEntityList.get( 0 );
 
         } catch(SQLException e){
